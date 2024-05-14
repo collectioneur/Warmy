83ba3392576896f05e84b56733efb79557fdd075
Yehor Kharchenko <yehorkharchenko4@gmail.com>
2024-05-14 20:21:22 +0200

    fixed some issues

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
-const apiKey = "742e3849c7e24641a50e4649188afa4e";
+const apiKey = "";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?" +
   "units=metric&q=";
 
 const searchBox = document.querySelector(".input-search");
 const searchBtn = document.querySelector(".search-button")
 const weatherIcon = document.querySelector(".weather-icon")
-
-
+let time = "d";
+let previousCity = "";
 async function checkWeather(city) {
+  if(previousCity === city) return;
+  previousCity = city;
   const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
 
-  if (response.status == 404) {
+  if (response.status == 404 || response.status == 400) {
     document.querySelector(".error").style.display = "block";
     document.querySelector(".center").style.display = "none";
     document.querySelector(".bottom").style.display = "none";
@@ -25,7 +27,9 @@ async function checkWeather(city) {
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind-speed").innerHTML = data.wind.speed + " km/h";
 
-  if(data.weather[0].icon[2] == "d") {
+  time = data.weather[0].icon[2];
+  backgroundChange(time);
+  if(time == "d") {
   if (data.weather[0].main == "Clouds") {
     weatherIcon.src = "img/cloudnsun.png";
   } else if (data.weather[0].main == "Clear") {
@@ -63,6 +67,23 @@ async function checkWeather(city) {
   document.querySelector(".bottom").style.display = "flex";
   document.querySelector(".error").style.display = "none";
 }
+}
+
+function backgroundChange(time) {
+  const windowElement = document.querySelector(".window");
+  const nightGradient = "linear-gradient(133deg, rgba(4,18,47,1) 0%, rgba(127,2,134,1) 100%)";
+  const dayGradient = "linear-gradient(63deg, rgba(213,148,252,1) 0%, rgba(148,233,213,1) 100%)";
+
+  if(time === "n") {
+     if (windowElement.style.background !== nightGradient) {
+         windowElement.style.background = nightGradient;
+     }
+  }
+  else {
+    if (windowElement.style.background !== dayGradient) {
+      windowElement.style.background = dayGradient;
+    }
+  }
 }
 
 searchBtn.addEventListener("click", () => {
